2df332a5a98a610cd4bdbc31d8683f8f9d7644f5
GitCal <cahicolvictor@gmail.com>
2024-05-17 17:55:46 -0300

    modfcç

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TimeDifference from './TimeDifference'
+
+function App() {
+  return(
+    <div className="App">
+      <TimeDifference/>
+      </div>
+  )
+}
+
+export default App
